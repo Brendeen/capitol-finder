61213fd71f8c5aff8295e13e0b7c08ba5543e148
Brenden Moore <brendenm3603@gmail.com>
2023-05-10 20:16:27 -0700

    test 2

--- api/country.py
+++ api/country.py
@@ -1,12 +1,39 @@
-from http.server import BaseHTTPRequestHandler
-from datetime import datetime
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from urllib import parse
+import requests
 
 
 class handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        url = self.path
+        url_components = parse.urlsplit(url)
+        query_string_list = parse.parse_qsl(url_components.query)
+        dictionary = dict(query_string_list)
 
-  def do_GET(self):
-    self.send_response(200)
-    self.send_header('Content-type', 'text/plain')
-    self.end_headers()
-    self.wfile.write(str(datetime.now().strftime('%Y-%m-%d %H:%M:%S')).encode())
-    return
+        capital = dictionary.get('capital')
+        countries_api_url = "https://restcountries.com/v3.1/all"
+        response = requests.get(countries_api_url)
+        data = response.json()
+        # print("data is:", data)
+        country = next((x for x in data if x.get('capital') == capital), None)
+
+        if country:
+            capital_name = country['capital'][0]
+            country_name = country['name']['common']
+            message = f"The capital of {country_name} is {capital_name}."
+        else:
+            message = f"Sorry, could not find a country with capital {capital}."
+        print("message is:", message)
+
+        # Forming the response
+        self.send_response(200)  # HTTP code
+        self.send_header('Content-type', 'text/plain')  # define the content type
+        self.end_headers()  # add a blank line
+        self.wfile.write(message.encode())  # write the message
+
+
+if __name__ == '__main__':
+    server_address = ('localhost', 8000)  # use any available port
+    httpd = HTTPServer(server_address, handler)  # httpd is a commonly used abbreviation for "HTTP Daemon"
+    print(f'Starting httpd server on {server_address[0]}:{server_address[1]}')
+    httpd.serve_forever()
