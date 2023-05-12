9e5638dde06b7b42679827310bd9e06b36bb7fbb
Brenden Moore <brendenm3603@gmail.com>
2023-05-11 20:10:14 -0700

    final commit

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # capitol-finder
+
+Working example urls:
+
+- http://localhost:8000/countries?query=Chile
+- http://localhost:8000/countries?query=Spain
+- http://localhost:8000/countries?query=Sudan
+- http://localhost:8000/countries?query=Macau
+- http://localhost:8000/countries?query=Equatorial Guinea
--- api/country.py
+++ api/country.py
@@ -6,14 +6,20 @@ import requests
 class handler(BaseHTTPRequestHandler):
     def do_GET(self):
         url = self.path
+        # print('url', url)
         url_components = parse.urlsplit(url)
         query_string_list = parse.parse_qsl(url_components.query)
-        dictionary = dict(query_string_list)
+        # print('query_string_list', query_string_list)
+        dic = dict(query_string_list)
+        # print('dictionary', dic)
 
-        country = dictionary.get('country')
+        country = dic.get('query')
+        # print('country', country)
         countries_api_url = f"https://restcountries.com/v3.1/name/{country}"
+        print("countries_api_url", countries_api_url)
         response = requests.get(countries_api_url)
         data = response.json()
+        print(f'this is the data:', data)
         # country = next((x for x in data if x.get('capital') == country), None)
 
         if country:
@@ -31,7 +37,7 @@ class handler(BaseHTTPRequestHandler):
 
 
 if __name__ == '__main__':
-    server_address = ('localhost', 8000)  # use any available port
-    httpd = HTTPServer(server_address, handler)  # httpd is a commonly used abbreviation for "HTTP Daemon"
+    server_address = ('localhost', 8000)
+    httpd = HTTPServer(server_address, handler)
     print(f'Starting httpd server on {server_address[0]}:{server_address[1]}')
     httpd.serve_forever()
